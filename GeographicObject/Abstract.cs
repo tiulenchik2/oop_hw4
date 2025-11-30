906b643c6e72ccb9a51557da74a05cc31768ffc5
Illia Moskaliuk <74979403+tiulenchik2@users.noreply.github.com>
2025-11-30 18:08:00 +0200

    Second part of homework

---
+++ GeographicObject/Abstract.cs
@@ -0,0 +1,46 @@
+﻿public abstract class GeographicObject
+{
+    private int X;
+    private int Y;
+    private string Name;
+    private string Description;
+    public GeographicObject(int x, int y, string name, string description)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+    }
+    public virtual string GetInfo()
+    {
+        return $"Name: {Name}, Description: {Description}, Coordinates: ({X}, {Y})";
+    }
+}
+class River : GeographicObject
+{
+    private int FlowSpeed;
+    private int Length;
+    public River(int x, int y, string name, string description, int flowSpeed, int length)
+        : base(x, y, name, description)
+    {
+        FlowSpeed = flowSpeed;
+        Length = length;
+    }
+    public override string GetInfo()
+    {
+        return base.GetInfo() + $", Flow Speed: {FlowSpeed} m/s, Length: {Length} km";
+    }
+}
+class Mountain : GeographicObject
+{
+    private int HighestPoint;
+    public Mountain(int x, int y, string name, string description, int highpoint, string mountainType)
+        : base(x, y, name, description)
+    {
+        HighestPoint = highpoint;
+    }
+    public override string GetInfo()
+    {
+        return base.GetInfo() + $", Highest point: {HighestPoint} m";
+    }
+}
---
+++ GeographicObject/Interface.cs
@@ -0,0 +1,67 @@
+﻿public interface IGeographicObject
+{
+    string GetInfo();
+}
+
+public class River_Interface : IGeographicObject
+{
+    private readonly int X;
+    private readonly int Y;
+    private readonly string Name;
+    private readonly string Description;
+
+    private readonly int FlowSpeed;
+    private readonly int Length;
+
+    public River_Interface(int x, int y, string name, string description, int flowSpeed, int length)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+        FlowSpeed = flowSpeed;
+        Length = length;
+    }
+
+    public string GetInfo()
+    {
+        return $"Name: {Name}, Descriptiom: {Description}, Coordinates: ({X}, {Y}), Flow speed: {FlowSpeed} m/s, Length: {Length} km";
+    }
+}
+
+public class Mountain_Interface : IGeographicObject
+{
+    private readonly int X;
+    private readonly int Y;
+    private readonly string Name;
+    private readonly string Description;
+
+    private readonly int HighestPoint;
+
+    public Mountain_Interface(int x, int y, string name, string description, int highestPoint)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+        Description = description;
+        HighestPoint = highestPoint;
+    }
+
+    // Реалізація методу GetInfo()
+    public string GetInfo()
+    {
+        return $"Name: {Name}, Descriptiom: {Description}, Coordinates: ({X}, {Y}), Highest point: {HighestPoint} m";
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+
+        IGeographicObject dnister = new River_Interface(25, 45, "Danube", "River that flows along borde", 3, 1362);
+        IGeographicObject roman_kosh = new Mountain_Interface(45, 34, "Roman-kosh", "Highest point of Crimea", 1545);
+
+        Console.WriteLine(dnister.GetInfo());
+        Console.WriteLine(roman_kosh.GetInfo());
+    }
+}
